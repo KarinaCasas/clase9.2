3caa5fd527ae51a8dda4762cd8a5b67286a7578c
KarinaC <karinacasasj@gmail.com>
2022-09-11 22:05:46 -0300

    Hice prompts para crear un document write con javascript

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var nombre;
+var edad;
+var comida;
+nombre = prompt("ingresa tu nombre");
+edad = prompt("ingresa tu edad");
+comida = prompt("ingresa tu comida favorita");
+
+document.write(nombre + " " + "tiene " + edad + " " + "y ama comer " + comida);
